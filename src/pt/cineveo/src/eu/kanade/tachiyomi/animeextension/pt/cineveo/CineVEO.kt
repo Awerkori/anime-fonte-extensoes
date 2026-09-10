ab4be4cf0e04f210e20a3ecb67e4ae4a61b9c51b
Awerkori <118941394+Awerkori@users.noreply.github.com>
2026-09-10 06:18:16 -0300

    fix(pt): enforce CineVEO hero element type

--- src/pt/cineveo/src/eu/kanade/tachiyomi/animeextension/pt/cineveo/CineVEO.kt
+++ src/pt/cineveo/src/eu/kanade/tachiyomi/animeextension/pt/cineveo/CineVEO.kt
@@ -113,7 +113,7 @@ class CineVEO : AnimeHttpLegacySource() {
 
     override fun animeDetailsParse(response: Response): SAnime {
         val document = response.asJsoup()
-        val hero: Element = document.selectFirst(".series-hero-v2, .movie-hero-v2") ?: document
+        val hero = (document.selectFirst(".series-hero-v2, .movie-hero-v2") ?: document) as Element
         return SAnime.create().apply {
             setUrlWithoutDomain(document.location())
             title = hero.selectFirst("h1")?.text() ?: document.title().substringBefore(" - CineVEO")
